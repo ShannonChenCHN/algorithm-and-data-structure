7df7b0bd80c9d4fa79b8b85036e790c82fd7f897
ShannonChenCHN <273879688@qq.com>
2018-03-28 19:58:49 +0800

    更新算法练习题

--- Practices/CodingInterview/15_NumberOf1InBinary/main.cpp
+++ Practices/CodingInterview/15_NumberOf1InBinary/main.cpp
@@ -8,8 +8,48 @@
 
 #include <iostream>
 
+
+// 110101
+
+int NumberOfNumOneInBinary(int num) {
+    
+    int count = 0;
+    
+    while (num) {
+        num = (num - 1) & num;
+        count++;
+    }
+    
+    return count;
+}
+
+void Test(const char *testName, int number, int expected) {
+    
+    printf("\n============\n");
+    
+    if (testName != nullptr) {
+        printf("%s Begins.", testName);
+    }
+    
+    int result = NumberOfNumOneInBinary(number);
+    
+    if (result == expected) {
+        printf("\nTest Passed.\n");
+    } else {
+        printf("\nTest Failed.\n");
+    }
+    
+    printf("\n============\n");
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    // 4,  100
+    Test("Test1", 4, 1);
+    
+    // 5, 101
+    Test("Test2", 5, 2);
+    
+    
     return 0;
 }
